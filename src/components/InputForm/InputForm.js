b180eb3f7d46b4d33b9250a44d47b0e745c40e30
Nikki Aaron <nikki@ameronix.com>
2018-03-29 17:39:53 -0400

    deleted duplicate folder

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,30 +1,77 @@
 import React, {Component} from 'react';
+import Draft from 'draft-js';
+import CodeUtils from 'draft-js-code';
 
-class LoginForm extends Component {
+class InputForm extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      username: "",
-      password: ""
-    }
-    this.handleFormInput = this.handleFormInput.bind(this);
+      editorState: Draft.EditorState.createEmpty()
+    };
   }
 
-  handleFormInput(event) {
-    const {name, value} = event.target;
-    this.setState({[name]: value});
+  onChange = (editorState) => {
+    this.setState({
+      editorState
+    })
+  }
+
+  handleKeyCommand = (command) => {
+    const { editorState } = this.state;
+    let newState;
+
+    if (CodeUtils.hasSelectionInBlock(editorState)) {
+      newState = CodeUtils.handleKeyCommand(editorState, command);
+    }
+
+    if (!newState) {
+      newState = RichUtils.handleKeyCommand(editorState, command);
+    }
+
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  keyBindingFn = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return Draft.getDefaultKeyBinding(evt);
+
+    const command = CodeUtils.getKeyBinding(evt);
+
+    return command || Draft.getDefaultKeyBinding(evt);
+  }
+
+  handleReturn = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
+
+    this.onChange(CodeUtils.handleReturn(evt, editorState));
+    return 'handled';
+  }
+
+  onTab = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
+
+    this.onChange(CodeUtils.onTab(evt, editorState));
+    return 'handled';
   }
 
   render() {
     return (
-      <form>
-        <input type="text" name="username" onChange={this.handleFormInput} value={this.state.username} />
-        <input type="password" name="password" onChange={this.handleFormInput} value={this.state.password} />
-        <button type="submit">Login</button>
-      </form>
-    )
+      <Draft.Editor
+        editorState={this.state.editorState}
+        onChange={this.onChange}
+        keyBindingFn={this.keyBindingFn}
+        handleKeyCommand={this.handleKeyCommand}
+        handleReturn={this.handleReturn}
+        onTab={this.onTab}
+      />
+    );
   }
 }
 
-export default LoginForm;
+export default InputForm;
