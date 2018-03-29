fe6743d1ab65288d919bdd457292d62b9fe73a11
Nikki Aaron <nikki@ameronix.com>
2018-03-29 18:45:34 -0400

    added draft-js-code, deleted duplicate public folder

--- client/src/components/InputForm/InputForm.js
+++ client/src/components/InputForm/InputForm.js
@@ -1,22 +1,78 @@
 import React, {Component} from 'react';
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils, getDefaultKeyBinding} from 'draft-js';
+import CodeUtils from 'draft-js-code';
 import "./InputForm.css";
 
-class InputForm extends Component {
+class InputForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = editorState => this.setState({ editorState });
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
   }
+ 
+  onChange = (editorState) => {
+    this.setState({
+      editorState
+    })
+  }
+ 
+  handleKeyCommand = (command) => {
+    const { editorState } = this.state;
+    let newState;
+ 
+    if (CodeUtils.hasSelectionInBlock(editorState)) {
+      newState = CodeUtils.handleKeyCommand(editorState, command);
+    }
+ 
+    if (!newState) {
+      newState = RichUtils.handleKeyCommand(editorState, command);
+    }
+ 
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+ 
+  keyBindingFn = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return getDefaultKeyBinding(evt);
+ 
+    const command = CodeUtils.getKeyBinding(evt);
+ 
+    return command || getDefaultKeyBinding(evt);
+  }
+ 
+  handleReturn = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
+ 
+    this.onChange(CodeUtils.handleReturn(evt, editorState));
+    return 'handled';
+  }
+ 
+  onTab = (evt) => {
+    const { editorState } = this.state;
+    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
+ 
+    this.onChange(CodeUtils.onTab(evt, editorState));
+    return 'handled';
+  }
+ 
   render() {
     return (
-      <div id="content">
-        <h1>Draft.js Editor</h1>
+      <div className="content">
+        <h1>Draft.js</h1>
         <div className="editor">
           <Editor
-            className="inputForm"
             editorState={this.state.editorState}
             onChange={this.onChange}
+            keyBindingFn={this.keyBindingFn}
+            handleKeyCommand={this.handleKeyCommand}
+            handleReturn={this.handleReturn}
+            onTab={this.onTab}
           />
         </div>
       </div>
