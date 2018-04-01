54cba06e0388f945ba89f40c804dc42d0fb53038
Nikki Aaron <nikki@ameronix.com>
2018-04-01 18:24:30 -0400

    input form has buttons and weird code functionality

--- client/src/components/InputForm/InputForm.js
+++ client/src/components/InputForm/InputForm.js
@@ -1,88 +1,108 @@
 import React, {Component} from 'react';
-import {Editor, EditorState, RichUtils, getDefaultKeyBinding} from 'draft-js';
-import CodeUtils from 'draft-js-code';
-import PrismDecorator from 'draft-js-prism';
+import {EditorState, RichUtils} from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createCodeEditorPlugin from 'draft-js-code-editor-plugin';
+import createToolbarPlugin, { Separator } from 'draft-js-static-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+import createPrismPlugin from 'draft-js-prism-plugin';
 import Prism from 'prismjs';
+import './../../../node_modules/draft-js/dist/Draft.css';
+import './../../../node_modules/draft-js-prism/demo/editor.css';
+import  './../../../node_modules/draft-js-static-toolbar-plugin/lib/plugin.css';
+import './prism-okaidia.css';
 import "./InputForm.css";
 
-var decorator = new PrismDecorator({
-  // Provide your own instance of PrismJS
-  prism: Prism,
+var editorPlugin = createCodeEditorPlugin();
+var prismPlugin = createPrismPlugin({ prism: Prism });
+var toolbarPlugin = createToolbarPlugin({
+  structure: [
+    ItalicButton,
+    BoldButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton,
+  ]
 });
-var editorState = EditorState.createEmpty(decorator)
+
+var { Toolbar } = toolbarPlugin;
+
+var plugins = [
+  editorPlugin,
+  toolbarPlugin,
+  prismPlugin
+];
+
+var editorState = EditorState.createEmpty();
+
+var text = 'In this editor you can type text and code …';
 
 class InputForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty(decorator)
+      editorState    
+    };
+    this.onChange = (editorState) => {
+      this.setState({
+        editorState,          
+      });
     };
   }
- 
-  onChange = (editorState) => {
-    this.setState({
-      editorState
-    })
-  }
- 
-  handleKeyCommand = (command) => {
-    const { editorState } = this.state;
-    let newState;
- 
-    if (CodeUtils.hasSelectionInBlock(editorState)) {
-      newState = CodeUtils.handleKeyCommand(editorState, command);
+  
+  focus = () => {
+    this.editor.focus();
+  };
+
+  getBlockStyle = (block) => {
+    switch (block.getType()) {
+      case 'code-block': return 'language-javascript';
+      default: return null;
     }
- 
-    if (!newState) {
-      newState = RichUtils.handleKeyCommand(editorState, command);
-    }
- 
+  };
+
+  handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
       this.onChange(newState);
       return 'handled';
     }
     return 'not-handled';
-  }
- 
-  keyBindingFn = (evt) => {
-    const { editorState } = this.state;
-    if (!CodeUtils.hasSelectionInBlock(editorState)) return getDefaultKeyBinding(evt);
- 
-    const command = CodeUtils.getKeyBinding(evt);
- 
-    return command || getDefaultKeyBinding(evt);
-  }
- 
-  handleReturn = (evt) => {
-    const { editorState } = this.state;
-    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
- 
-    this.onChange(CodeUtils.handleReturn(evt, editorState));
-    return 'handled';
-  }
- 
-  onTab = (evt) => {
-    const { editorState } = this.state;
-    if (!CodeUtils.hasSelectionInBlock(editorState)) return 'not-handled';
- 
-    this.onChange(CodeUtils.onTab(evt, editorState));
-    return 'handled';
-  }
- 
+  };
+   
   render() {
     return (
-      <div className="content">
+      <div className="inputForm">
         <h1>Draft.js</h1>
-        <div className="editor">
+        <div className="editor" onClick={this.focus}>
           <Editor
             editorState={this.state.editorState}
             onChange={this.onChange}
-            keyBindingFn={this.keyBindingFn}
+            plugins={plugins}
+            blockStyleFn={this.getBlockStyle}
             handleKeyCommand={this.handleKeyCommand}
-            handleReturn={this.handleReturn}
-            onTab={this.onTab}
+            ref={(element) => { this.editor = element; }}
           />
         </div>
+        <Toolbar />
       </div>
     );
   }
