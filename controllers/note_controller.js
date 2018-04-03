dd35d3aabd057eca63f3990fea0cb77e98bd9d6e
Aaron <aaronl.brinkley@gmail.com>
2018-04-03 14:57:30 -0400

    Styling

--- controllers/note_controller.js
+++ controllers/note_controller.js
@@ -12,55 +12,26 @@ router.get('/postNote', (req, res) => {
 router.get('/notes/read/', (req, res) => {
   db.notes.findAll().then( data => {
     res.json("reading");
-    // if (data) {
-    //   data.updateAttributes({verified: true});
-    //   req.flash('loginMessage', '<p class="success-message text-center">Email verified. Please login.</p>');
-    //   res.redirect('/');
-    // } else {
-    //   req.flash('loginMessage', '<p class="error-message text-center">Invalid verification token. Please contact support.</p>');
-    //   res.redirect('/');
-    // }
+    
   });
 });
 
 router.get('/notes/read/:id', (req, res) => {
     db.notes.findOne({where: {id: req.params.id}}).then( data => {
       res.json("reading");
-      // if (data) {
-      //   data.updateAttributes({verified: true});
-      //   req.flash('loginMessage', '<p class="success-message text-center">Email verified. Please login.</p>');
-      //   res.redirect('/');
-      // } else {
-      //   req.flash('loginMessage', '<p class="error-message text-center">Invalid verification token. Please contact support.</p>');
-      //   res.redirect('/');
-      // }
+      
     });
   });
 
 router.post('/note/create/', (req, res) => {
   db.notes.findOne({where: {id: req.params.id}}).then(data => {
     res.json("creating");
-    // if (data) {
-    //   if (data.dataValues.verified) {
-    //     let token = randomstring.generate(16);
-    //     data.updateAttributes({verificationToken: token});
-    //     req.flash('resetMessage', '<p class="success-message text-center">Password reset email sent.</p>');
-    //   } else {
-    //     //User account is not verified, can't reset password
-    //     req.flash('resetMessage', 
-    //       `<p class="error-message text-center">Account is not verified. Please verify email before reseting password.
-    //       <a href="/resendVerification/${data.dataValues.username}>Resend Verification</a></p>`);
-    //   }
-    // } else {
-    //   req.flash('resetMessage', '<p class="error-message text-center">Username does not match our records</p>');
-    // }
-
-    // res.redirect('/resetRequest');
+    
   });
 });
-//Route to edit note will 
+
 router.put('/notes/update/:id', (req, res) => {
-  // update one of the burgers
+  
   db.notes.update({
     body: req.body
   },
@@ -78,50 +49,11 @@ router.put('/notes/update/:id', (req, res) => {
 router.delete('/notes/delete/:id', (req, res) => {
     db.notes.findOne({where: {id: req.params.id}}).then(data => {
         res.json("deleting");
-      // if (data) {
-      //   if (data.dataValues.verified) {
-      //     let token = randomstring.generate(16);
-      //     data.updateAttributes({verificationToken: token});
-      //     req.flash('resetMessage', '<p class="success-message text-center">Password reset email sent.</p>');
-      //   } else {
-      //     //User account is not verified, can't reset password
-      //     req.flash('resetMessage', 
-      //       `<p class="error-message text-center">Account is not verified. Please verify email before reseting password.
-      //       <a href="/resendVerification/${data.dataValues.username}>Resend Verification</a></p>`);
-      //   }
-      // } else {
-      //   req.flash('resetMessage', '<p class="error-message text-center">Username does not match our records</p>');
-      // }
-  
-      // res.redirect('/resetRequest');
+      
+      
     });
   });
 
-// router.get('/reset/:username/:token', (req, res) => {
-//   db.users.findOne({where: {username: req.params.username, verificationToken: req.params.token}}).then(data =>{
-//     if (data) {
-//       res.render('resetForm');
-//     } else {
-//       req.flash('loginMessage', '<p class="error-message">Invalid password reset token. Please contact support.</p>');
-//       res.redirect('/');
-//     }
-//   });
-// });
 
-// router.post('/reset/:username/:token', (req, res) => {
-//   db.users.findOne({where: {username: req.params.username, verificationToken: req.params.token}}).then(data =>{
-//     if (data) {
-//       data.updateAttributes({
-//         password: bcrypt.hashSync(req.body.password, null, null),
-//         verificationToken: randomstring.generate(16)
-//       });
-//       req.flash('loginMessage', '<p class="success-message text-center">Password reset succesful. Please login with new password.</p>');
-//       res.redirect('/');
-//     } else {
-//       req.flash('loginMessage', '<p class="error-message text-center">Invalid password reset token. Please contact support.</p>');
-//       res.redirect('/');
-//     }
-//   });
-// });
 
 module.exports = router;
