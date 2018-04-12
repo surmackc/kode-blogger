ee7e85e3a280efe190a58181941b4e3623a4d1f9
Tristan <tristan@flatland.co>
2018-04-12 19:56:14 -0400

    styling changes to home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -29,18 +29,32 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        Home content here...
-        <Carousel />
+        {/* Home content here...
+        <Carousel /> */}
         <Link to="/displaypost"><button className="btn btn-outline-dark mr-2">View Post</button></Link>
         <Link to="/posts/all"><button className="btn btn-outline-dark">All Posts</button></Link>
+        <div className="recent-post">
+          <h2><span id="recent-post-title">recent</span><span id="recent-post-title-second">Posts</span><span id="recent-post-curly">&#123;</span></h2>
+        
+        <div className="recent-post-container1">
         {this.state.notes.map(note => {
           return (
-          <div key={note.id}>
-          <span>{note.title}</span>
+          <div className="recent-post-container">
+          <div id="home-recent-post-block" key={note.id}>
+          <div className="home-recent-post-title-container">
+          <span id="home-recent-post-title">{note.title}</span>
+          </div>
           <div className="html-output" dangerouslySetInnerHTML={{__html: html.serialize(note.body, {sanitize: true})}} >
+          </div>
+          <Link to="/displaypost"><button className="btn btn-outline-dark">View Post</button></Link>
           </div>
           </div>);
         })}
+        
+      </div>
+      </div>
+
+      <h2><span id="recent-post-curly-end">&#125;</span></h2>
       </div>
     )
   }
