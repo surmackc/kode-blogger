aced188df76818db57db233299a1d12eb5c8a755
Smack <surmackc@gmail.com>
2018-04-13 12:03:44 -0400

    added view post buttons to search results, changed btn-success to match navbar

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import PostListItem from '../PostListItem/PostListItem';
 import postApi from '../../utils/postAPI';
+import {Redirect} from 'react-router-dom';
+
 
 class SearchResults extends Component {
-  state = { results: [], searchString: "" }
+  state = { results: [], searchString: "", redirect: "" }
 
   componentDidMount() {
     if (this.props.match.params.search) {
@@ -27,20 +29,29 @@ class SearchResults extends Component {
     this.doSearch(this.state.searchString);
   }
 
+  viewPost = (id) => {
+    this.setState({redirect: <Redirect to={`/posts/${id}`} /> })
+  }
+
   render() {
     return (
       <div>
+        {this.state.redirect ? this.state.redirect : ''}
         <h2><span id="recent-post-title">search</span><span id="recent-post-curly">&#123;</span></h2>
         <form style={{width: "100%"}} className="form-inline d-flex justify-content-center mb-5">
           <input style={{width: "60%"}} className="form-control" type="text" 
             value={this.state.searchString} 
             onChange={this.onSearchChanged}/>
-          <button className="btn btn-success ml-2" type="submit" onClick={this.onSearchSubmit}>Search</button>
+          <button className="btn btn-outline-success ml-2" type="submit" onClick={this.onSearchSubmit}>Search</button>
         </form>
       <ul className="list-group">
       {this.state.results.length ? 
-        this.state.results.map(result => <PostListItem key={result.id} {...result}/>) 
-        : <div>No Results</div>}
+        this.state.results.map(result => 
+        <PostListItem key={result.id} {...result}>
+        <button className="btn btn-outline-dark" type="button" onClick={() => this.viewPost(result.id)}>View Post</button>
+        </PostListItem>
+        ) 
+        : <div className="text-center">No Results</div>}
       </ul>
       <h2><span id="recent-post-curly-end">&#125;</span></h2>
       </div>
