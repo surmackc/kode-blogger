84fec8f59c03a1d6840f1bd08fef1af7873e0547
David Friend <david@ameronix.com>
2018-04-14 13:20:30 -0400

    Changed postlistitem to be more responsive, mainly in manage posts

--- client/src/components/PostListItem/PostListItem.js
+++ client/src/components/PostListItem/PostListItem.js
@@ -9,13 +9,17 @@ const PostListItem = ({title, id, created, updated, children}) => (
           <div className="home-post-list-title-container">
             <span id="home-post-list-title">{title}</span>
           </div>
-          <div className="d-flex justify-content-end">
-            <div className="mr-auto">
-              {created ? <p>Created at {new Date(created).toString()}</p> : ''}
-              {updated ? <p>Modified at {new Date(updated).toString()}</p> : ''}
+          <div className="row">
+            <div className="col-12 ml-auto">
+              {created ? <p>Created {new Date(created).toLocaleDateString()}</p> : ''}
+              {updated ? <p>Modified {new Date(updated).toLocaleDateString()}</p> : ''}
             </div>
-          {children}
-          </div> 
+          </div>
+          <div className="row ml-auto">
+            <div className="col-12">
+              {children}
+            </div>  
+          </div>
         </div>       
       </div>
     </div>
