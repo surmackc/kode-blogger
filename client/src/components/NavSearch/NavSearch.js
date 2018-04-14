d7d13a15e9ada3ef628a071e91b8cca61b7a413c
David Friend <david@ameronix.com>
2018-04-14 12:50:53 -0400

    Fixed search button position when on small screen size

--- client/src/components/NavSearch/NavSearch.js
+++ client/src/components/NavSearch/NavSearch.js
@@ -15,7 +15,7 @@ class NavSearch extends Component {
       <li className="nav-item ml-auto">
       <form className="form-inline my-0">
         <input onChange={this.onSearchInput} value={this.searchString} className="form-control ml-2" type="search" placeholder="Search" aria-label="Search" />
-        <Link to={`/search/${this.state.searchString}`}>
+        <Link className="ml-auto" to={`/search/${this.state.searchString}`}>
         <button className="btn btn-outline-light" type="submit">Search</button>
         </Link>
       </form>
