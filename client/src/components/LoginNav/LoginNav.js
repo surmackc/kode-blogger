5b4f58df7b7fb9a20f5a4bb8d02e9a207a1f0b04
David Friend <david@ameronix.com>
2018-04-14 13:45:43 -0400

    More navbar changes to fix some issues I introducted

--- client/src/components/LoginNav/LoginNav.js
+++ client/src/components/LoginNav/LoginNav.js
@@ -4,17 +4,13 @@ import { Link } from 'react-router-dom'
 const LoginNav = (props) => 
         props.loggedIn ? 
           <div className="nav-item ml-auto">
-            <span className="text-light mr-1 ml-1">{props.username}</span>
-            <button className="btn btn-outline-light" onClick={props.logOut} type="button">Logout</button> 
+            <span className="text-light ml-2">{props.username}</span>
+            <button className="btn btn-outline-light ml-2" onClick={props.logOut} type="button">Logout</button> 
           </div>
           :
           (<div className="nav-item ml-auto">
-            <li>
-            <Link to="/signup"><button className="btn btn-outline-light" type="button">Signup</button></Link>
-            </li>
-            <li>
-            <Link to="/login"><button className="btn btn-outline-light" type="button">Login</button></Link>
-            </li>
+              <Link to="/signup"><button className="btn btn-outline-light" type="button">Signup</button></Link>
+              <Link to="/login"><button className="btn btn-outline-light" type="button">Login</button></Link>
           </div>);
 
 export default LoginNav;
