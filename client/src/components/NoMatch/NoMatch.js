3d137c213ef03c35b245fb0ad8dcfd9d085cdc4f
David Friend <david@ameronix.com>
2018-04-17 16:10:17 -0400

    Changed 404 page

--- client/src/components/NoMatch/NoMatch.js
+++ client/src/components/NoMatch/NoMatch.js
@@ -3,13 +3,9 @@ import React, {Component} from "react";
 class NoMatch extends Component {
   render() {
     return (  
-      <div className="jumbotron">
+      <div className="text-center">
         <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
+        <h4>Sorry, we couldn't find what you're looking for.</h4>
       </div>
     )
   }
